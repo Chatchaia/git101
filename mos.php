e43578e023789b5dd028a43a5addfed997483c30
Chatchai <kmitl50ite15engineer@gmail.com>
2017-08-12 22:02:43 +0700

    modify mos.php

--- mos.php
+++ mos.php
@@ -1,1 +1,16 @@
-ECHO is on.
+<?php
+
+    foreach ($customers as $key => $customer) 
+    {
+      $temp = array();
+      $temp['ip_add']       = $customer->ip_address;
+      $temp['country_name'] = $customer->country_name;
+      $temp['region_code']  = $customer->region_code;
+      $temp['region_name']  = $customer->region_name;
+      $temp['city']         = $customer->city;
+      $temp['lat_user']     = $customer->latitude_user;
+      $temp['long_user']    = $customer->longitude_user;
+      $result[] = $temp;
+    }
+
+?>
